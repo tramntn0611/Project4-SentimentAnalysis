d4a7ef2ffd9b5d7851a9719f05019e9c919573a4
tramntn0611 <dstramntn@gmail.com>
2024-06-15 11:35:13 +0700

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -17,6 +17,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import nltk
 from nltk.tokenize import sent_tokenize
 from underthesea import word_tokenize, pos_tag, sent_tokenize
+from wordcloud import WordCloud
+
 
 
 nltk.download('punkt')
